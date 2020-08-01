685f280aa1e42938894e198221902d0b9f9e8c08
Oscar Cruz <Oscararmcruz25@gmail.com>
2020-08-01 15:28:44 -0500

    layout

---
+++ script.js
@@ -0,0 +1,9 @@
+// Grab current day and hour from moment
+// Color the blocks based on time
+// Loop through the time blocks
+// If it's in the past, grey, if present, red, if future green
+// if (currentHour > the id of the timeblock), add the past class
+
+// $("#9")
+
+// Localstorage to store all the inputs from the textareas
